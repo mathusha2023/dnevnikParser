2cfb3970c7b11672151181c2362a1cbc68a430dd
Alexey <bazooka2008@inbox.ru>
2025-05-10 11:17:14 +0300

    aaa

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests as r
 from bs4 import BeautifulSoup
 import sys
 
-url = "https://dnevnik.admin-smolensk.ru/journal-student-grades-action"
+url = "https://dnevnik.admin-smolensk.ru/journal-student-grades-action?mode=print"
 
 headers = {"Content-Type": "application/octet-stream",
            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"}
@@ -18,4 +18,37 @@ if status_code != 200:
     sys.exit()
 
 parsed_html = BeautifulSoup(resp.content, "html.parser")
-print(parsed_html)
+
+# получаем названия всех предметов
+subjects = list(map(lambda x: x.text.strip(), parsed_html.find_all("div", class_="text-overflow lhCell offset16")))
+
+d = {}
+
+for subject in subjects:
+    marks_list = []
+    marks_html = parsed_html.find_all("div", attrs={"name": subject})[1:]  # первый элемент это название предмета, нам нужны только сами оценки
+    for mark in marks_html:
+        mark = mark.text.strip().replace("\n", "").split(" ")[0]  # получаем непосредственно оценку (в т.ч. 4✕3) или Н
+        if mark and mark != "Н":
+            marks_list.append(mark)
+    d[subject] = marks_list
+
+for sub in d:
+    marks = d[sub]
+    summa = 0
+    count = 0
+    for str_mark in marks:
+        if len(str_mark) == 1:
+            summa += int(str_mark)
+            count += 1
+        else:
+            m = int(str_mark[0])
+            k = int(str_mark[2])
+            summa += m * k
+            count += k
+
+    print(sub)
+    print(*marks)
+    if count:
+        print(f"Средняя: {round(summa / count, 2)}")
+    print()
